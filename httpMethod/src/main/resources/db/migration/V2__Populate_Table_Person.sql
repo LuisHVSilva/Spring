2cd09be7d5e4ae66ec0a99471bbfe9c9884e7e38
LuisHVSilva <lhvs1412@hotmail.com>
2023-09-20 16:50:23 -0300

    Adding migrations with flyway plugin

---
+++ httpMethod/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+LOCK TABLES `person` WRITE;
+INSERT INTO `person` VALUES
+(1,'Name: 1','Last Name: 1', 'Brasil 1', 'Male'),
+(2,'Name: 2','Last Name: 2', 'Brasil 2', 'Male'),
+(3,'Name: 3','Last Name: 3', 'Brasil 3', 'Male');
+UNLOCK TABLES;
